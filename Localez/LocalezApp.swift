8b1a4bc931684cb2b05a42e84531e374ced39a81
Leo Wehrfritz <leo@wehrfritz.me>
2026-05-17 14:19:41 +0200

    Initial Commit

---
+++ Localez/LocalezApp.swift
@@ -0,0 +1,17 @@
+//
+//  LocalezApp.swift
+//  Localez
+//
+//  Created by Leo Wehrfritz on 17.05.26.
+//
+
+import SwiftUI
+
+@main
+struct LocalezApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ LocalezTests/LocalezTests.swift
@@ -0,0 +1,18 @@
+//
+//  LocalezTests.swift
+//  LocalezTests
+//
+//  Created by Leo Wehrfritz on 17.05.26.
+//
+
+import Testing
+
+struct LocalezTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
